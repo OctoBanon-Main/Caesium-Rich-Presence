d41e426e036b78017dec3d32a54dbd2a56f11b4e
OctoBanon <82901151+OctoBanon-Main@users.noreply.github.com>
2022-04-27 13:44:55 +0700

    Double space after [LOG] or [ERROR] fix

--- include/messages.py
+++ include/messages.py
@@ -6,7 +6,7 @@ def cleaner():
 
 
 def colorizer(r, g, b, text):
-    return '\033[38;2;{};{};{}m{} \033[38;2;255;255;255m'.format(r, g, b, text)
+    return '\033[38;2;{};{};{}m{}\033[38;2;255;255;255m'.format(r, g, b, text)
 
 
 # Errors
